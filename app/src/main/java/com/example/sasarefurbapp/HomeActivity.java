00b7b4ad53c67e571a1ea187f4860cb522042961
emma1997s <emmamaabi@gmail.com>
2021-12-26 23:10:00 +0300

    inital changes

--- app/src/main/java/com/example/sasarefurbapp/HomeActivity.java
+++ app/src/main/java/com/example/sasarefurbapp/HomeActivity.java
@@ -2,12 +2,17 @@ package com.example.sasarefurbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.Objects;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements  View.OnClickListener {
 
+    //initialise the imgView
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +20,14 @@ public class HomeActivity extends AppCompatActivity {
 
         //Remove the App ActionBar
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
