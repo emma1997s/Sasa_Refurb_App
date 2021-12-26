51c079112ddce486c43ef23ee6726c298ff080f4
emma1997s <emmamaabi@gmail.com>
2021-12-26 13:58:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sasarefurbapp/Login.java
@@ -0,0 +1,14 @@
+package com.example.sasarefurbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/sasarefurbapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sasarefurbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Remove actionBar menu
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Create intent for the splashScreen and the next Activity
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+        }, 10000);
+    }
+}
